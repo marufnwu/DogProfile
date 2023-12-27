8ec89cb949d97dff33e51e34f17ad5f1179baf97
Maruf-ASUS <dev00.maruf@gmail.com>
2023-12-27 16:12:41 +0600

    Design using composer constraint layout

--- app/src/main/java/com/maruf/dogprofile/ProfilePage.kt
+++ app/src/main/java/com/maruf/dogprofile/ProfilePage.kt
@@ -1,9 +1,12 @@
 package com.maruf.dogprofile
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -27,95 +30,157 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
+private const val TAG = "ProfilePage"
 @Composable
 fun ProfilePage(){
     Card(modifier = Modifier
         .fillMaxSize()
-        .padding(20.dp)) {
-        ConstraintLayout(
-            modifier = Modifier
-                .verticalScroll(rememberScrollState())
-                .padding(10.dp)
-                .fillMaxSize(),
-        ){
-            val (profileImage, nameText, countryName, statRow, actionRow) = createRefs()
-            Image(painter = painterResource(id = R.drawable.dog), contentDescription = "Doggy",
-                modifier = Modifier
-                    .size(size = 100.dp)
-                    .clip(CircleShape)
-                    .border(width = 2.dp, color = Color.Red, shape = CircleShape)
-                    .constrainAs(profileImage){
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        bottom.linkTo(parent.bottom)
-                    }
-                , )
-            Text(
-                text = "Doggy",
-                color = Color.Green,
-                fontFamily = FontFamily.Monospace,
-                fontSize = 26.sp,
-                //modifier = Modifier.background(color = Color.Red),
-                fontStyle = FontStyle.Normal,
-                modifier = Modifier.constrainAs(nameText){
-                    top.linkTo(profileImage.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-            )
-            Text(text = "Bangladesh",  modifier = Modifier.constrainAs(countryName){
-                top.linkTo(nameText.bottom)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            })
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp).constrainAs(statRow){
-                    top.linkTo(countryName.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-                horizontalArrangement = Arrangement.SpaceEvenly,
-            ) {
-
-                ProfileStat(value = "120", title = "Followers")
-                ProfileStat(value = "100", title = "Following")
-                ProfileStat(value = "10", title = "Posts")
-
-
+        .padding(20.dp)
+        .verticalScroll(rememberScrollState())) {
+        BoxWithConstraints {
+            val constraint = if(minWidth<600.dp){
+                portraitConstraint()
+            }else{
+                landescapeConstraint()
             }
+            ConstraintLayout(constraint){
+                Image(painter = painterResource(id = R.drawable.dog), contentDescription = "Doggy",
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .clip(CircleShape)
+                        .border(width = 2.dp, color = Color.Red, shape = CircleShape)
+                        .layoutId("profileImage")
+                    , )
+                Text(
+                    text = "Doggy",
+                    color = Color.Green,
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 26.sp,
+                    //modifier = Modifier.background(color = Color.Red),
+                    fontStyle = FontStyle.Normal,
+                    modifier = Modifier.layoutId("nameText"),
+                )
+                Text(text = "Bangladesh",  modifier = Modifier.layoutId("countryName"))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .layoutId("statRow"),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                ) {
 
-            Row(
-                modifier = Modifier.fillMaxWidth().constrainAs(actionRow){
-                    top.linkTo(statRow.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Follow User")
+                    ProfileStat(value = "120", title = "Followers")
+                    ProfileStat(value = "100", title = "Following")
+                    ProfileStat(value = "10", title = "Posts")
+
+
                 }
 
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Direct Message")
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .layoutId("actionRow"),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Follow User")
+                    }
+
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Direct Message")
+                    }
                 }
             }
         }
     }
 
+}
+
+fun portraitConstraint() : ConstraintSet{
+    Log.d(TAG, "portraitConstraint: ")
+    return ConstraintSet {
+        val profileImage = createRefFor("profileImage")
+        val nameText = createRefFor("nameText")
+        val countryName = createRefFor("countryName")
+        val statRow = createRefFor("statRow")
+        val actionRow = createRefFor("actionRow")
+
+        constrain(profileImage){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            bottom.linkTo(parent.bottom)
+        }
+        constrain(nameText){
+            top.linkTo(profileImage.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+        constrain(countryName){
+            top.linkTo(nameText.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+        constrain(statRow){
+            top.linkTo(countryName.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+        constrain(actionRow){
+            top.linkTo(statRow.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+    }
+}
+fun landescapeConstraint() : ConstraintSet{
+    return ConstraintSet {
+        val profileImage = createRefFor("profileImage")
+        val nameText = createRefFor("nameText")
+        val countryName = createRefFor("countryName")
+        val statRow = createRefFor("statRow")
+        val actionRow = createRefFor("actionRow")
+
+        constrain(profileImage){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+        }
+        constrain(nameText){
+            top.linkTo(profileImage.bottom)
+            start.linkTo(profileImage.start)
+            end.linkTo(profileImage.end)
+        }
+        constrain(countryName){
+            top.linkTo(nameText.bottom)
+            start.linkTo(nameText.start)
+            end.linkTo(nameText.end)
+        }
+        constrain(statRow){
+            top.linkTo(parent.top)
+            start.linkTo(profileImage.end)
+            end.linkTo(parent.end)
+        }
+        constrain(actionRow){
+            top.linkTo(statRow.bottom)
+            start.linkTo(actionRow.end)
+            end.linkTo(parent.end)
+        }
+
+    }
 }
 
 @Composable
